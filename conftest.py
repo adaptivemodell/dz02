b24a69eacf4558de9771a4627ebdde153a8c5fa3
adaptivemodell <48nice@gmail.com>
2023-09-21 15:14:36 +0300

    Code change

--- conftest.py
+++ conftest.py
@@ -2,12 +2,12 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture()
+@pytest.fixture(scope='function')
 def browser_open():
     browser.config.window_width = 1024
     browser.config.window_height = 768
     browser.open('https://google.com')
 
-    yield
+    yield browser
 
-    # print("1111111")
+    browser.quit()
--- test_dz2.py
+++ test_dz2.py
@@ -10,4 +10,4 @@ def test_siteopen(browser_open):
 def test_siteopen2(browser_open):
     browser.element('[name="q"]').should(be.blank).type('ergf43g3g43q3ggggg232').press_enter()
     browser.element('[class="mnr-c"]').should(have.text("По запросу ergf43g3g43q3ggggg232 ничего не найдено."))
-    print('test_siteopen2')
+    #print('test_siteopen2')
